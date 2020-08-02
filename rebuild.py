adcd79f172b66f42d045231ebc46d579535cf6f9
Claire <clahsu@gmail.com>
2020-08-01 17:47:04 -0700

    update build script

--- rebuild.py
+++ rebuild.py
@@ -8,4 +8,4 @@ print("build from new context...")
 os.system("docker build -t portfolio:v1 .")
 
 print("run new container...")
-os.system("docker run --name portfolio -d -p 8080:80 portfolio:v1")
+os.system("docker run --name portfolio -d -p 80:80 portfolio:v1")
