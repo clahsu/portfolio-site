3b9498d6f34d8acb002881b702d56fa4367a0a91
Claire <clahsu@gmail.com>
2020-08-01 17:38:29 -0700

    initial upload portfolio websites

---
+++ rebuild.py
@@ -0,0 +1,11 @@
+import os, sys
+from shutil import which
+
+print("Kill running portolio containers")
+os.system("docker kill portfolio")
+
+print("build from new context...")
+os.system("docker build -t portfolio:v1 .")
+
+print("run new container...")
+os.system("docker run --name portfolio -d -p 8080:80 portfolio:v1")
